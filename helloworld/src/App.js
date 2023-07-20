037654c94c48b241cef9beb082e9c0bacaf31669
rahul bobade <rahul@tekvision.in>
2023-07-20 12:26:31 +0530

    Upload file

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -12,23 +12,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={logo} className="App-logo" alt="logo" />
         <div>
           {showText ? <h1>Thank you</h1> : <h1>Hello World</h1>}
-          <button>
-            <a href="#link" onClick={handleClick}>
-              Tap Here
-            </a>
-          </button>
+          <button aria-label='Tap Here'href="#link" onClick={handleClick} tabIndex={0} >Tap Here</button>
         </div>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+          <a className="App-link" aria-label="Tekvision Link" href="https://tekvision.in/">Link</a>
       </header>
     </div>
   );
