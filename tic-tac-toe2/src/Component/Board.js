192453e7ce533d0f249a2dd5ae4a64951127175c
rahul <rahul@tekvision.in>
2023-07-25 16:15:36 +0530

    New updated file with implimenting accessiblity

--- tic-tac-toe2/src/Component/Board.js
+++ tic-tac-toe2/src/Component/Board.js
@@ -8,15 +8,15 @@ export const Board = ({ board, onClick }) => {
   return (
     <div className='board' role="grid" aria-label="table 3 row and 3 column">
         {board.map(( value, idx ) => {
-          const row = Math.floor(idx /3) +1;
-          const col = (idx% 3)+ 1;
+          // const row = Math.floor(idx /3) +1;
+          // const col = (idx% 3)+ 1;
           // const arialabel= `${row} row ${col} col`;
-          const arialabel= value ? value : `${row} row ${col} col`;
+          // const arialabel= value ? value : ` ${row} row ${col} col`;
 
             return( 
             <Box key={idx} value={value} index={idx}
             onClick={()=> value === null && onClick(idx)} 
-            aria-label={arialabel}/>
+            />
             );
         })}
     </div>
--- tic-tac-toe2/src/Component/Box.js
+++ tic-tac-toe2/src/Component/Box.js
@@ -5,12 +5,12 @@ import "./Box.css"
 
 export const Box = ({ value,onClick,index, ...rest }) => {
     const style= value === "X" ? "box x" : "box o";
-    const cellLabel= value ? value : `${Math.floor(index/3)+ 1} row ${(index% 3 + 1)} col`;
+    const cellLabel=`${Math.floor(index/3)+ 1} row ${(index% 3 + 1)} col`;
     // const label= value ? value : `Box ${index + 1}`;
-    
+    // console.log(rest)
     return (
     <button className={style} onClick={onClick} {...rest} 
-    roll="gridcell" aria-label={cellLabel}>
+    roll="gridcell" aria-label={cellLabel+value}>
       {value}
       </button>
   );
